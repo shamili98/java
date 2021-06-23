314effe271a2526fcf66134bc741f412e186686f
shamili <shamili1098@gmail.com>
2021-06-23 12:13:45 +0530

    updated hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("Hello all, Nagraj & shamili");
+      System.out.println("Hello welcome to devops, Nagraj & shamili");
    }
 }

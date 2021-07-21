a5882d7f934ac1069900fe8a3387797f17c8325a
shamili <shamili1098@gmail.com>
2021-07-21 18:47:01 +0530

    updated hello world file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("welcome all to the winning team,Shamili,Sapna,Mohan,Thirthankar");
+      System.out.println("Hi all to the winning team,Shamili,Sapna,Mohan,Thirthankar");
    }
 }
